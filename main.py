1b6737cf81ddcf7b57d53fb9f3293845e0df89f7
iomelvil <ianomelville@gmail.com>
2020-01-28 15:34:41 -0500

    finish deck build, starting player

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 #hello world
 #Blackjack
-#todo create deck of cards- get creation from Hanabi Code
-#ad graphic user interface
+#todo add shuffle deck method from hanabi code and draw card
+import random
 
-class card:
+class Card:
     def __init__(self, suit, number):
         self.s = suit #suite
         self.n = number #number
 
     def show(self):
-        print("{} of {}".format(self.n, self.s))
+        if self.n == 1:
+            print("{} of {}".format("Ace", self.s))
+        if self.n < 11 and self.n > 1:
+            print("{} of {}".format(self.n, self.s))
+        if self.n == 11:
+            print("{} of {}".format("Jack", self.s))
+        if self.n == 12:
+            print("{} of {}".format("Queen", self.s))
+        if self.n == 13:
+            print("{} of {}".format("King", self.s))
 
-class player:
-    def __init__(self):
-        pass
+class Player:
+    def __init__(self, name):
+        self.hand = hand[]
+        self.name = name
 
-class deck:
+class Deck:
     def __init__(self):
         self.cards = []
         self.build()
 
     def build(self):
-        for s in ["Spades"]
-card = card("Clubs", "Ace")
-card.show()
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for n in range(1, 14):
+                self.cards.append(Card(s, n))
+
+    def show(self):
+        for card in self.cards:
+            card.show()
+
+deck = Deck()
+deck.show()
+
+
+
