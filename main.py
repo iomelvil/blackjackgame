3e3f779153e833875ebd9ff0eaf3620b0a23d2d4
iomelvil <ianomelville@gmail.com>
2020-01-28 11:23:46 -0500

    starting to builddeck

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #hello world
+#Blackjack
+#todo create deck of cards- get creation from Hanabi Code
+#ad graphic user interface
+
+class card:
+    def __init__(self, suit, number):
+        self.s = suit #suite
+        self.n = number #number
+
+    def show(self):
+        print("{} of {}".format(self.n, self.s))
+
+class player:
+    def __init__(self):
+        pass
+
+class deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades"]
+card = card("Clubs", "Ace")
+card.show()
